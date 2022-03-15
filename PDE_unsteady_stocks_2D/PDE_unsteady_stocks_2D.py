95038b723f93b5ce545b50b448cd9230901a5d55
hj <528802953@qq.com>
2022-03-15 15:27:36 +0800

    add

--- PDE_unsteady_stocks_2D/PDE_unsteady_stocks_2D.py
+++ PDE_unsteady_stocks_2D/PDE_unsteady_stocks_2D.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from torch.autograd import Variable
 
 PDE_datafile = ''
 PDE_dim = 3
@@ -22,27 +23,33 @@ alpha = 0.025
 
 
 def f1(t, x1, x2):
+    t = Variable(t, requires_grad=True)
+    x1 = Variable(x1, requires_grad=True)
+    x2 = Variable(x2, requires_grad=True)
     u1 = PDE_u1(t, x1, x2)
     p = PDE_p(t, x1, x2)
-    u1_t = torch.autograd.grad(u1, t, create_graph=True)[0]
-    u1_x1 = torch.autograd.grad(u1, x1, create_graph=True)[0]
-    u1_x1x1 = torch.autograd.grad(u1_x1, x1, create_graph=True)[0]
-    u1_x2 = torch.autograd.grad(u1, x2, create_graph=True)[0]
-    u1_x2x2 = torch.autograd.grad(u1_x2, x2, create_graph=True)[0]
-    p_x1 = torch.autograd.grad(p, x1, create_graph=True)[0]
+    u1_t = torch.autograd.grad(u1, t,grad_outputs=torch.ones_like(u1),create_graph=True)[0]
+    u1_x1 = torch.autograd.grad(u1, x1,grad_outputs=torch.ones_like(u1), create_graph=True)[0]
+    u1_x1x1 = torch.autograd.grad(u1_x1, x1,grad_outputs=torch.ones_like(u1_x1), create_graph=True)[0]
+    u1_x2 = torch.autograd.grad(u1, x2,grad_outputs=torch.ones_like(u1), create_graph=True)[0]
+    u1_x2x2 = torch.autograd.grad(u1_x2, x2, grad_outputs=torch.ones_like(u1_x2),create_graph=True)[0]
+    p_x1 = torch.autograd.grad(p, x1, grad_outputs=torch.ones_like(p),create_graph=True)[0]
     f = u1_t - 0.025 * (u1_x1x1 + u1_x2x2) + p_x1
     return f
 
 
 def f2(t, x1, x2):
+    t = Variable(t, requires_grad=True)
+    x1 = Variable(x1, requires_grad=True)
+    x2 = Variable(x2, requires_grad=True)
     u2 = PDE_u2(t, x1, x2)
     p = PDE_p(t, x1, x2)
-    u2_t = torch.autograd.grad(u2, t, create_graph=True)[0]
-    u2_x1 = torch.autograd.grad(u2, x1, create_graph=True)[0]
-    u2_x1x1 = torch.autograd.grad(u2_x1, x1, create_graph=True)[0]
-    u2_x2 = torch.autograd.grad(u2, x2, create_graph=True)[0]
-    u2_x2x2 = torch.autograd.grad(u2_x2, x2, create_graph=True)[0]
-    p_x2 = torch.autograd.grad(p, x2, create_graph=True)[0][:1]
+    u2_t = torch.autograd.grad(u2, t, grad_outputs=torch.ones_like(u2),create_graph=True)[0]
+    u2_x1 = torch.autograd.grad(u2, x1,grad_outputs=torch.ones_like(u2), create_graph=True)[0]
+    u2_x1x1 = torch.autograd.grad(u2_x1, x1,grad_outputs=torch.ones_like(u2_x1), create_graph=True)[0]
+    u2_x2 = torch.autograd.grad(u2, x2, grad_outputs=torch.ones_like(u2),create_graph=True)[0]
+    u2_x2x2 = torch.autograd.grad(u2_x2, x2,grad_outputs=torch.ones_like(u2_x2), create_graph=True)[0]
+    p_x2 = torch.autograd.grad(p, x2,grad_outputs=torch.ones_like(p), create_graph=True)[0][:1]
     f = u2_t - 0.025 * (u2_x1x1 + u2_x2x2) + p_x2
     return f
 
